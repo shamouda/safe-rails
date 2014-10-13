f133bde2c4229a7aee672157fda08d8e816d221b
Ubuntu <ubuntu@ip-10-217-19-194.us-west-2.compute.internal>
2014-10-13 05:58:58 +0000

    updating scripts

--- scripts/reset-pg.sh
+++ scripts/reset-pg.sh
@@ -1,5 +1,13 @@
 
+sudo chown -R ubuntu /var/run/postgresql
+
 pg_ctl stop
 rm -rf /mnt/external/PGDATA
 pg_ctl init -D /mnt/external/PGDATA
-/usr/lib/postgresql/9.3/bin/postgres -D /mnt/external/PGDATA
+echo "shared_buffers = 30GB" >> /mnt/external/PGDATA/postgresql.conf
+echo "host all all             all           trust" >> /mnt/external/PGDATA/pg_hba.conf
+{ /usr/lib/postgresql/9.3/bin/postgres -D /mnt/external/PGDATA & }
+sleep 2; psql postgres -c "CREATE USER rails WITH PASSWORD 'rails'"; psql postgres -c "CREATE DATABASE rails"; psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE rails to rails";
+cd ../demo
+rake db:migrate
+wait
--- scripts/setup-pg.sh
+++ scripts/setup-pg.sh
@@ -1,4 +1,8 @@
 
+echo "export PATH=$PATH:/usr/lib/postgresql/9.3/bin/" >> ~/.bashrc
+echo "export PGDATA=/mnt/external/PGDATA" >> ~/.bashrc
+source ~/.bashrc
+
 sudo mkfs.ext4 /dev/xvdc
 sudo mkdir /mnt/external
 sudo mount /dev/xvdc /mnt/external

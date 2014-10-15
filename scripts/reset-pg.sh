11648a811235d5c0a34700d8e8eca272529a1098
Ubuntu <ubuntu@ip-10-217-19-194.us-west-2.compute.internal>
2014-10-15 23:35:24 +0000

    address not all in pg_hba

--- scripts/reset-pg.sh
+++ scripts/reset-pg.sh
@@ -14,7 +14,7 @@ rm -rf /mnt/external/PGDATA
 pg_ctl init -D /mnt/external/PGDATA
 echo "shared_buffers = 30GB" >> /mnt/external/PGDATA/postgresql.conf
 echo "listen_addresses '*'" >> /mnt/external/PGDATA/postgresql.conf
-echo "host all all             all           trust" >> /mnt/external/PGDATA/pg_hba.conf
+echo "host all all             0.0.0.0/0          trust" >> /mnt/external/PGDATA/pg_hba.conf
  /usr/lib/postgresql/9.3/bin/postgres -D /mnt/external/PGDATA & 
 sleep 2; psql postgres -c "CREATE USER rails WITH PASSWORD 'rails'"; psql postgres -c "CREATE DATABASE rails"; psql postgres -c "GRANT ALL PRIVILEGES ON DATABASE rails to rails";
 cd ~/safe-rails/demo

3d07da6096932dd32b56ec541e007b3139bd924f
Ubuntu <ubuntu@ip-10-221-3-29.us-west-2.compute.internal>
2014-10-17 22:11:11 +0000

    update bench scripts

--- bench/bench.py
+++ bench/bench.py
@@ -6,13 +6,19 @@ import pickle
 PG_HOST = "ec2-54-203-221-73.us-west-2.compute.amazonaws.com"
 RAILS_HOST = "ec2-54-245-166-54.us-west-2.compute.amazonaws.com"
 
+use_nginx = True
+
 def reset_hosts(nprocs):
     print "Starting PG"
     reset_postgres(PG_HOST)
     sleep(4)
     print "Starting rails"
-    start_passenger(RAILS_HOST, nprocs)
-    sleep(2)
+    if not use_nginx:
+        start_passenger(RAILS_HOST, nprocs)
+        sleep(2)
+    else:
+        start_unicorn(RAILS_HOST, nprocs)
+        sleep(10)
 
 print "Starting bench"
 
@@ -21,30 +27,32 @@ fk_models = ["simple", "belongs_to", "dbfk"]
 
 STRESS_RAILS_WORKERS = [1, 2, 4, 8, 16, 32, 64]
 
-BENCH_PK_STRESS = False
-BENCH_FK_STRESS = False
+BENCH_PK_STRESS = True
+BENCH_FK_STRESS = True
 BENCH_PK_WORKLOAD = True
+BENCH_FK_WORKLOAD = True
 
 STRESS_CLIENTS = 64
-STRESS_TRIALS = 10
-iterations = 3
+STRESS_TRIALS = 100
+iterations = 2
 
 FK_USERS_TO_DEPT_PROPORTION = 10
 
-NUMRECORDS = [1, 10, 100, 1000, 10000]
-OPS_PER_CLIENT = 1
+NUMRECORDS_PK = [1, 10, 100, 1000, 10000, 100000]
+NUMRECORDS_FK = [1, 10, 100, 1000, 10000, 100000]
+OPS_PER_CLIENT = 100
 WORKLOADS = ["ycsb", "linkbench-ins", "linkbench-upd", "uniform"]
 RECORD_TEST_N_RAILS_PROCS = 64
 RECORD_TEST_N_CLIENTS =64
 
-OUTFILE = "results.pkl"
+OUTFILE = "/mnt/external/results.pkl"
 
 ALL_RESULTS = []
 
 if BENCH_PK_WORKLOAD:
     for it in range(0, iterations):
         for workload in WORKLOADS:
-            for records in NUMRECORDS:
+            for records in NUMRECORDS_PK:
                 pk_bench_model_results = {}
                 pk_bench_dups = {}
                 for m in pk_models:
@@ -61,7 +69,7 @@ if BENCH_PK_WORKLOAD:
                     pk_bench_dups[m] = count_duplicates(PG_HOST, m)
                     print pk_bench_dups
                     ALL_RESULTS.append({ 
-                        "bench":"PK_STRESS",
+                        "bench":"PK_WORKLOAD",
                         "workload":workload,
                         "records":records,
                         "model":m,
@@ -72,6 +80,7 @@ if BENCH_PK_WORKLOAD:
                         "results": pk_bench_model_results,
                         "dups" : pk_bench_dups
                     })  
+                    pickle.dump(ALL_RESULTS, open(OUTFILE, 'w'))
 
 if BENCH_PK_STRESS:
     for it in range(0, iterations):
@@ -93,7 +102,41 @@ if BENCH_PK_STRESS:
                 "results": pk_stress_model_results,
                 "dups" : pk_stress_dups
                 })
+                pickle.dump(ALL_RESULTS, open(OUTFILE, 'w'))
 
+if BENCH_FK_WORKLOAD:
+    for it in range(0, iterations):
+        for workload in ["unused"]:
+            for records in NUMRECORDS_FK:
+                fk_bench_model_results = {}
+                fk_bench_dups = {}
+                for m in fk_models:
+                    print "STARTING", m, "..."
+                    print RECORD_TEST_N_RAILS_PROCS, RECORD_TEST_N_CLIENTS
+                    reset_hosts(RECORD_TEST_N_RAILS_PROCS)
+
+                    fk_bench_model_results[m] = fk_workload(RAILS_HOST,
+                                                            records=records, 
+                                                            model=m,
+                                                            ops_per_client=OPS_PER_CLIENT,
+                                                            n_clients=RECORD_TEST_N_CLIENTS,
+                                                            users_to_dept=FK_USERS_TO_DEPT_PROPORTION)
+                    fk_bench_dups[m] = count_dangling_users(PG_HOST, m)
+                    print fk_bench_dups
+                    ALL_RESULTS.append({ 
+                        "bench":"FK_WORKLOAD",
+                        "users_to_dept":FK_USERS_TO_DEPT_PROPORTION,
+                        "records":records,
+                        "model":m,
+                        "ops_per_client":OPS_PER_CLIENT,
+                        "iteration":it,
+                        "rails_procs":RECORD_TEST_N_RAILS_PROCS,
+                        "clients":RECORD_TEST_N_CLIENTS,
+                        "results": fk_bench_model_results,
+                        "dups" : fk_bench_dups
+                    })  
+
+                    pickle.dump(ALL_RESULTS, open(OUTFILE, 'w'))
 
 if BENCH_FK_STRESS:
     for it in range(0, iterations):
@@ -103,7 +146,7 @@ if BENCH_FK_STRESS:
             for m in fk_models:
                 print "STARTING", m, "..."
                 reset_hosts(nprocs)
-                fk_stress_model_results[m] = fk_stress(RAILS_HOST, model=m, nclients=STRESS_CLIENTS, trials=STRESS_TRIALS)
+                fk_stress_model_results[m] = fk_stress(RAILS_HOST, model=m, n_clients=STRESS_CLIENTS, trials=STRESS_TRIALS)
                 fk_stress_dups[m] = count_dangling_users(PG_HOST, m)
                 print fk_stress_dups[m]
                 ALL_RESULTS.append({ 
@@ -116,7 +159,7 @@ if BENCH_FK_STRESS:
                     "dangling" : fk_stress_dups
             })
 
-pickle.dump(ALL_RESULTS, open(OUTFILE, 'w'))
+            pickle.dump(ALL_RESULTS, open(OUTFILE, 'w'))
 
 exit(0)
 

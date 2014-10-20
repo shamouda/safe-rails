96b06c2615317b5e75547cdaad0d00fcd5a81ec4
Peter Bailis <pbailis@gmail.com>
2014-10-20 09:58:52 -0700

    updating submodules

---
+++ ext/_scripts/update_submodules.py
@@ -0,0 +1,21 @@
+from os import listdir, system, walk, path
+import fnmatch, itertools
+from collections import defaultdict
+
+DO_CHECK_AUTHORS = True
+DO_CHECK_LINES = True
+
+def find_files(directory, pattern):
+    for root, dirs, files in walk(directory):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+                filename = path.join(root, basename)
+                if True:#filename.find("/test/") == -1:
+                    yield filename
+
+for proj in listdir('.'):
+    if proj == "_scripts":
+        continue
+
+    system("cd "+proj+"; git checkout master; git pull")
+ 
